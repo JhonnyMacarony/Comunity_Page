9a034a39c24d7207c58738cc0da494d9456ccb29
JhonnyMacarony <91407912+JhonnyMacarony@users.noreply.github.com>
2021-12-03 19:23:02 +0200

    first commit

--- server.js
+++ server.js
@@ -8,9 +8,29 @@ const expressLayouts = require("express-ejs-layouts")
 
 const indexRouter = require('./routes/index')
 
+app.get("/", (req, res) =>{
+    res.render("layouts/layout.ejs")
+})
+
+app.get("/event", (req, res) =>{
+    res.render("layouts/event.ejs")
+})
+
+app.get("/news", (req, res) =>{
+    res.render("layouts/news.ejs")
+})
+
+app.get("/contact", (req, res) =>{
+    res.render("layouts/contact.ejs")
+})
+
+app.get("/settings", (req, res) =>{
+    res.render("layouts/settings.ejs")
+})
+
 app.set("view engine", "ejs")
 app.set("views", __dirname + '/views')
-app.set('layout', 'layouts/layout')
+app.set('layout', 'layouts/event')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
